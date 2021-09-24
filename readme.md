a787a27d1a4485240a134ed833397052cc890324
user <user@support.com>
2021-09-24 11:00:41 +0800

    add readme.md

---
+++ readme.md
@@ -0,0 +1,28 @@
+# readme
+
+```
+git clone https://github.com/pengjichen/atom_test.git
+
+cd atom_test
+
+npm i
+
+```
+
+## 自动转账
+
+转账金额默认0.1 需要根据具体情况自行修改
+```
+node atom_transfer_loop_v3_clear_balance.js
+```
+
+
+## 抢先转账
+
+转账金额默认0.1 需要根据具体情况自行修改
+启动时间和停止时间需要根据情况自行修改
+```
+node atom_transfer_loop_v2_grabbing.js
+```
+
+
--- util.js
+++ util.js
@@ -5,7 +5,7 @@ async function sleep(time) {
 }
 exports.sleep = sleep;
 
-function readKeyFromFile(path){
+async function readKeyFromFile(path){
         let keyLine = fs.readFileSync(path).toString();
         if (keyLine.charAt(keyLine.length-1) == '\n'){
             return keyLine.slice(0,keyLine.length-1);
@@ -16,3 +16,18 @@ function readKeyFromFile(path){
 
 exports.readKeyFromFile = readKeyFromFile;
 
+async function Timer(timestampTarget, func){
+    setTimeout(func, timestampTarget)
+}
+
+exports.Timer = Timer;
+
+// 北京时间格式: let str = "2021-09-23 20:15:00+08:00 --> utc time:  2021-09-23T12:15:00.000Z
+// utc时间格式: let str = "2021-09-23 12:15:00 --> utc time:  2021-09-23T12:15:00.000Z
+// return millisecond
+async function toUtcTimestamp(str){
+    let t = new Date(str)
+    return t.getTime();
+}
+
+exports.toUtcTimestamp = toUtcTimestamp
