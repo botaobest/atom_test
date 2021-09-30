a7c2ee344285550112dea9c6a1be0a8c0ff47d60
pengjichen <pengjichen@126.com>
2021-09-30 15:16:50 +0800

    test1 time

--- atom_transfer_loop_v2_grabbing.js
+++ atom_transfer_loop_v2_grabbing.js
@@ -159,8 +159,12 @@ async function timerToGrab(){
 
     let now = Date.now();
 
-    let beginTime = await util.toUtcTimestamp("2021-10-05 12:47:08+08:00");
-    let endTime   = await util.toUtcTimestamp("2021-10-05 12:51:56+08:00");
+    //let beginTime = await util.toUtcTimestamp("2021-10-05 12:47:08+08:00");
+    //let endTime   = await util.toUtcTimestamp("2021-10-05 12:51:56+08:00");
+
+    let beginTime = await util.toUtcTimestamp("2021-09-30 15:30:00+08:00");
+    let endTime   = await util.toUtcTimestamp("2021-09-30 15:32:00+08:00");
+
 
     // 开始抢
     util.Timer(beginTime - now, grabing, [mnemonic]);
