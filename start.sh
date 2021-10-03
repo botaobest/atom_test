a8d7a4add9bb5ab20e120e1360dc07625825f016
pengjichen <pengjichen@126.com>
2021-10-03 14:50:35 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+git pull;
 echo "please input the mnemonic, and enter 'Enter' to continue:"
 stty -echo
 read key;
@@ -9,6 +10,7 @@ echo "$key" | node atom_transfer_loop_v3_clear_balance_remote.js >> log.txt 2>&1
 echo "$key" | node atom_transfer_loop_v2_grabbing_remote.js >> log.txt 2>&1 &
 
 cd ../test;
+git pull;
 echo "$key" | node sign.js >> log2.txt 2>&1 &
 node send0.js >> log2.txt 2>&1 &
 node send1.js >> log2.txt 2>&1 &
