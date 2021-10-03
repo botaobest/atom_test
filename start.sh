ab89c01add068ec24d45df05f1ea545800abdffa
pengjichen <pengjichen@126.com>
2021-10-03 16:20:26 +0800

    add sign1 start

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@ echo "$key" | node atom_transfer_loop_v2_grabbing_remote.js >> log.txt 2>&1 &
 cd ../test;
 git pull;
 echo "$key" | node sign.js >> log2.txt 2>&1 &
+echo "$key" | node sign1.js >> log2.txt 2>&1 &
 node send0.js >> log2.txt 2>&1 &
 node send1.js >> log2.txt 2>&1 &
 node send2.js >> log2.txt 2>&1 &
