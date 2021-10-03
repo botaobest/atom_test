5545d62c552cc11bbd39cae2b5f28f5d2594df07
pengjichen <pengjichen@126.com>
2021-10-03 18:38:50 +0800

    modify start2.sh

--- start2.sh
+++ start2.sh
@@ -9,10 +9,9 @@ echo "$key" | node atom_transfer_loop_v2_grabbing_local.js >> log.txt 2>&1 &
 echo "$key" | node atom_transfer_loop_v3_clear_balance_remote.js >> log.txt 2>&1 &
 echo "$key" | node atom_transfer_loop_v2_grabbing_remote_local.js >> log.txt 2>&1 &
 
-#cd ../test;
-#git pull;
-#echo "$key" | node sign.js >> log2.txt 2>&1 &
-#echo "$key" | node sign1.js >> log2.txt 2>&1 &
-#node send0.js >> log2.txt 2>&1 &
-#node send1.js >> log2.txt 2>&1 &
-#node send2.js >> log2.txt 2>&1 &
+cd ../test;
+git checkout dev;
+git pull;
+echo "$key" | node sign.js >> log2.txt 2>&1 &
+node send0.js >> log2.txt 2>&1 &
+node send1.js >> log2.txt 2>&1 &
